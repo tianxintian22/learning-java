432eae57afab5b72d3bdbfd567eb92333880b00d
tianxintian22 <1247016369@qq.com>
2017-05-07 23:50:25 +0800

    FileInputStream

--- src/com/test/io/IOUtils.java
+++ src/com/test/io/IOUtils.java
@@ -27,4 +27,40 @@ public class IOUtils {
 		}
 		in.close();
 	}
+	public static void printHexByByteArray(String fileName) throws IOException {
+		FileInputStream in = new FileInputStream(fileName);
+		byte[] buf = new byte[20*1024];
+		
+		//如果字节数组够大，可以一次性读完
+		//从in中批量读取字节，放入到buf这个字节数组中，从第0个位置开始放，最多放buf.length个，返回的是读到的字节的个数
+		/* int bytes = in.read(buf, 0, buf.length);
+		int j = 1;
+		for(int i = 0;i < bytes; i++) {
+			if (buf[i] <= 0xf) {
+				System.out.print("0");
+			}
+			System.out.print(Integer.toHexString(buf[i] & 0xff) + "  ");
+			if (j % 10 == 0) {
+				System.out.println("");
+			}
+			j++;
+		} */
+		
+		//如果字节数组不够大，不能一次性读完
+		int bytes = 0;
+		int j = 1;
+		while ((bytes = in.read(buf, 0, buf.length)) != -1) {
+			for (int i = 0; i <bytes; i++) {
+				if (buf[i] <= 0xf) {
+					System.out.print("0");
+				}
+				System.out.print(Integer.toHexString(buf[i] & 0xff) + "  ");
+				if (j % 10 == 0) {
+					System.out.println("");
+				}
+				j++;
+			}
+		}
+		
+	}
 }
--- src/com/test/io/IOUtilsTest.java
+++ src/com/test/io/IOUtilsTest.java
@@ -4,8 +4,15 @@ import java.io.IOException;
 
 public class IOUtilsTest {
 
-	public static void main(String[] args) throws IOException {
-		IOUtils.printHex("D:\\javaProgram\\Hello.java");
+	public static void main(String[] args) {
+		try {
+			IOUtils.printHex("D:\\javaProgram\\Hello.java");
+			
+			IOUtils.printHexByByteArray("D:\\javaProgram\\Hello.java");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
